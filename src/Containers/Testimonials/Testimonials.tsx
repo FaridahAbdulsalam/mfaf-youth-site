a7e5ceb98a936c5942845046e9a143af3cc6ad4d
Faridah Salami <f.salami@outlook.com>
2025-09-01 19:59:59 +0100

    slider functionality working

--- src/Containers/Testimonials/Testimonials.tsx
+++ src/Containers/Testimonials/Testimonials.tsx
@@ -7,8 +7,31 @@ import {
 import Title from "../../Components/Title/Title";
 import TestimonyCard from "../../Components/TestimonialCard/TestimonyCard";
 import { testimonyData } from "../../data";
+import { useRef } from "react";
 
 const Testimonials = () => {
+
+  const slider = useRef<HTMLUListElement | null> (null);
+  let tx = 0;
+
+  const handleSlideForward = () => {
+    if(tx > -50){
+      tx -= 25;
+  }
+    if(slider.current){
+      slider.current.style.transform = `translateX(${tx}%)`;
+    }
+  }
+
+  const handleSlideBackward = () => {
+    if(tx < 0){
+      tx += 25;
+    } 
+    if(slider.current){
+      slider.current.style.transform = `translateX(${tx}%)`;
+    }
+  }
+
   return (
     <>
       <Title title={"What Our Members Say"} subTitle={"Testimonials"} />
@@ -18,11 +41,12 @@ const Testimonials = () => {
           icon={faChevronRight}
           size="2xl"
           className="next-btn"
+          onClick={handleSlideForward}
         />
-        <FontAwesomeIcon icon={faChevronLeft} size="2xl" className="back-btn" />
+        <FontAwesomeIcon icon={faChevronLeft} size="2xl" className="back-btn" onClick={handleSlideBackward}/>
         
         <div className="slider">
-          <ul>
+          <ul ref={slider}>
             {testimonyData.map((item, index) => (
               <li key={index}>
                 <div className="slide">
